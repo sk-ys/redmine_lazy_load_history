37e18209200ab779542164b29a8dcea702c8b41d
sk-ys <yskrmt@gmail.com>
2026-05-07 12:59:06 +0900

    Implement user preferences for initial load and load count

--- lib/lazy_load_history.rb
+++ lib/lazy_load_history.rb
@@ -2,3 +2,5 @@ module LazyLoadHistory
 end
 
 require_relative 'lazy_load_history/issues_helper_patch'
+require_relative 'lazy_load_history/user_preference_patch'
+require_relative 'lazy_load_history/hooks'
